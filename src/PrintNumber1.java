21324cf599ee5c90aa02da54977e60be9d639104
AHMED AL ABRI <ahmedtechm@gmail.com>
2023-05-31 00:20:00 +0400

    Added ArrayList to print numbers in one line

---
+++ src/PrintNumber1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class PrintNumber1 {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+            System.out.println(numbers);
+
+    }
+}
